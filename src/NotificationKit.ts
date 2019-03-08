f7ec7203c8d8baedaa82760918b3829791bbb16a
awa2 <tawatsu@c-fo.com>
2019-03-08 16:01:04 +0900

    Modified renderHTML()

--- src/NotificationKit.ts
+++ src/NotificationKit.ts
@@ -122,11 +122,16 @@ namespace NotificationKit {
             let html = '';
 
             html = attachment.pretext ? `<p>${attachment.pretext.replace('\n', '<br>\n')}</p>\n` : '';
+
             if (attachment.author_name) {
                 html += attachment.author_link ? `<small><a href="${attachment.author_link}">${attachment.author_name}</a></small>` : `<small>${attachment.author_name}</small>`;
             }
-            html += `<h2>${attachment.title}</h2>`;
+            if (attachment.title) {
+                html += attachment.title_link ? `<h2><a href="${attachment.title_link}">${attachment.title}</a></h2>` : `<h2>${attachment.title}</h2>`;
+            }
+
             html += attachment.text ? `<p>${attachment.text.replace('\n', '<br>\n')}</p>` : '';
+
             if (attachment.fields) {
                 html += attachment.fields.map(field => {
                     return `<p><b>${field.title}</b><br>\n${field.value}</p>`;
